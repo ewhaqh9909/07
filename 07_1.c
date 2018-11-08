43607796b5c1692e057ec9cd915c4f94d9dae0c2
ewhaqh9909 <qhdud0921@naver.com>
2018-11-08 15:38:32 +0900

    07_1

---
+++ 07_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i;
+	i=365;
+	
+	int*ptr;
+	ptr=&i;
+	
+	printf("i의 주소:%i\n",&i);
+	printf("i의 값:%i\n",i);
+	
+	printf("ptr의 주소:%u\n",ptr);
+	printf("ptr의값:%i\n",*ptr);
+	return 0;
+}
